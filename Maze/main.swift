213b796af3834cef864220ea8a0d33c63131a494
Wygs45 <55256691+Wygs45@users.noreply.github.com>
2023-05-01 01:49:47 +0200

    Map and movement Implementation

---
+++ Maze/main.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let myMaze = Maze(x: 0, y: 0)
+let movment = Moves()
+print("Position du joueur", myMaze.playerPosition())
+movment.moveLeft(Maze: myMaze)
+print("Nouvelle position du joueur", myMaze.getPlayerPosition())
