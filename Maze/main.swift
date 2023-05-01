25e29a8a2bbf743cb5c3d6310855c4eafb67b71d
Wygs45 <55256691+Wygs45@users.noreply.github.com>
2023-05-01 18:28:00 +0200

    Files relocatied

--- Maze/main.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-let myMaze = Maze(x: 0, y: 0)
-let movment = Moves()
-print("Position du joueur", myMaze.playerPosition())
-movment.moveLeft(Maze: myMaze)
-print("Nouvelle position du joueur", myMaze.getPlayerPosition())
